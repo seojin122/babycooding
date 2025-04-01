14df55a422f6d1b5d34aa6ffcba6a4dd980938df
marin <akflsdlek23@naver.com>
2025-04-01 10:00:38 +0900

    (2025-03-14) success

---
+++ C++/1000.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    //cout << "A와 B값을 입력해주세요";
+    cin >> A >> B;
+
+    //cout << "A값은 " << A << "  B값은 " << B << endl;
+    cout << "A+B값은 " << A+B;
+    cout << A + B;
+
+    return 0;
+}
